ef09e8cee90f030315b05bddf508698fbf51c43c
Tonnerre LOMBARD <tonnerre@ancient-solutions.com>
2016-02-23 03:08:06 +0100

    Upgrade PostgreSQL version to 9.5.

--- network/geocolo-db/preparedb.sh
+++ network/geocolo-db/preparedb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-su postgres -c "nohup /usr/lib/postgresql/9.4/bin/postmaster -D /var/lib/postgresql/9.4/main      -c config_file=/etc/postgresql/9.4/main/postgresql.conf > /dev/null 2>&1 < /dev/null &"
+su postgres -c "nohup /usr/lib/postgresql/9.5/bin/postmaster -D /var/lib/postgresql/9.5/main      -c config_file=/etc/postgresql/9.5/main/postgresql.conf > /tmp/gen-postgresql.log 2>&1 < /dev/null &"
 attempts=60
 
 # Wait for PostgreSQL to come up.
@@ -10,13 +10,16 @@ do
 	attempts="$(expr "$attempts" - 1)"
 	if test "$attempts" = 0
 	then
+		cat /tmp/gen-postgresql.log
+		rm -f /tmp/gen-postgresql.log
 		exit 10
 	fi
 done
 
+ls -l /usr/share/postgresql/9.5/contrib/
 su postgres -c "createdb gis"
-su postgres -c "psql -f /usr/share/postgresql/9.4/contrib/postgis-2.1/postgis.sql gis"
-su postgres -c "psql -f /usr/share/postgresql/9.4/contrib/postgis-2.1/legacy.sql gis"
+su postgres -c "psql -f /usr/share/postgresql/9.5/contrib/postgis-2.2/postgis.sql gis"
+su postgres -c "psql -f /usr/share/postgresql/9.5/contrib/postgis-2.2/legacy.sql gis"
 su postgres -c "psql -f geoborders.sql gis"
 su postgres -c "psql -c \"CREATE ROLE georeader WITH NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN ENCRYPTED PASSWORD 'blahblahblubberblubber';\" postgres"
 su postgres -c "psql -c \"REVOKE CREATE ON SCHEMA public FROM georeader;\" gis"
@@ -32,6 +35,10 @@ do
 	attempts="$(expr "$attempts" - 1)"
 	if test "$attempts" = 0
 	then
+		cat /tmp/gen-postgresql.log
+		rm -f /tmp/gen-postgresql.log
 		exit 10
 	fi
 done
+
+rm -f /tmp/gen-postgresql.log
