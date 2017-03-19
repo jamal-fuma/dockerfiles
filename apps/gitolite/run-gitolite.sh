e0fe3ca2d3543bad1251e20f421773534b5f8614
Tonnerre LOMBARD <tonnerre@ancient-solutions.com>
2017-03-19 04:38:15 +0100

    Split the volumes for the SSH admin key and the SSH host keys.
    This allows to keep the admin key in a ConfigMap while the SSH host keys
    can be kept in a secret.

--- apps/gitolite/run-gitolite.sh
+++ apps/gitolite/run-gitolite.sh
@@ -36,9 +36,9 @@ install -o git -g git -m 0700 -d /var/lib/git/.gitolite	\
 	/var/lib/git/.gitolite/logs
 
 if [ ! -e /var/lib/git/.gitolite/keydir/pubkey.pub ] ||	\
-	! diff -q /secrets/pubkey.pub /var/lib/git/.gitolite/keydir/pubkey.pub
+	! diff -q /import/pubkey.pub /var/lib/git/.gitolite/keydir/pubkey.pub
 then
-	su - git -c 'gitolite setup -pk /secrets/pubkey.pub'
+	su - git -c 'gitolite setup -pk /import/pubkey.pub'
 fi
 
 install -o root -g root -m 0755 -d /etc/ssh
