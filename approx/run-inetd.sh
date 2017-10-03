77a214342e4fad6b897fd79974706bcb652a7814
Tonnerre LOMBARD <tonnerre@ancient-solutions.com>
2017-10-03 20:58:09 +0200

    Use openbsd-inetd for the approx container.
    micro-inetd has been removed from Debian and replaced with an empty
    package.

--- approx/run-inetd.sh
+++ approx/run-inetd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /usr/bin/install -o approx -g approx -m 0755 -d /srv/approx/cache
-ls -la /srv/approx/cache
 /usr/sbin/rsyslogd
 usermod -s /bin/bash "$SETUID_USER"
-exec su "$SETUID_USER" -c "exec /usr/bin/micro-inetd $APPROX_PORT /usr/sbin/approx"
+echo "$APPROX_PORT stream tcp nowait $SETUID_USER /usr/sbin/approx" > /etc/inetd.conf
+exec su "$SETUID_USER" -c "exec /usr/sbin/inetd -i /etc/inetd.conf"
