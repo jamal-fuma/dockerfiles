f4a7198647017f081a9c97c8e21856d8a42107eb
Tonnerre LOMBARD <tonnerre@ancient-solutions.com>
2017-11-27 04:23:36 +0100

    Add LDAP configuration for the Apache container if specified.
    This will be controlled via environment variables.

--- apps/ikiwiki/apache/run-ikiwiki.sh
+++ apps/ikiwiki/apache/run-ikiwiki.sh
@@ -9,6 +9,7 @@ do
     destdir="$(egrep '^[[:space:]]*destdir => ' $filename | awk -F\' '{ print $2 }')"
     libdir="$(egrep '^[[:space:]]*libdir => ' $filename | awk -F\' '{ print $2 }')"
     git_wrapper="$(egrep '^[[:space:]]*git_wrapper => ' $filename | awk -F\' '{ print $2 }')"
+    wikiname="$(egrep '^[[:space:]]*wikiname => ' $filename | awk -F\' '{ print $2 }')"
     git_dir="$(dirname "$(dirname "$git_wrapper")")"
 
     /usr/bin/install -o www-data -g git -m 0750 -d "$destdir" "$libdir"
@@ -17,8 +18,9 @@ do
         git clone "$git_dir" "$(basename "$srcdir")" && \
         /usr/bin/ikiwiki --setup "$filename")
 
-    cat << EOT > /etc/apache2/sites-available/$host.conf
-<VirtualHost _default_:8080>
+    output="$(mktemp -t apache-config-snippet.XXXXXX)"
+    echo "Writing temporary configuration to $output"
+    cat << EOT > $output
     ServerName $host
     DocumentRoot $destdir
 
@@ -28,42 +30,49 @@ do
         AllowOverride AuthConfig Limit
         Require all granted
     </Directory>
-</VirtualHost>
-<IfModule ssl_module>
-<VirtualHost _default_:8443>
-    ServerName $host
-    DocumentRoot $destdir
-
-    <Directory "$destdir">
-        Options ExecCGI FollowSymLinks
-        AddHandler cgi-script .cgi
-        AllowOverride AuthConfig Limit
-        Require all granted
-    </Directory>
-
-    SSLEngine on
-    SSLCertificateFile "/secret/tls.crt"
-    SSLCertificateKeyFile "/secret/tls.key"
-</VirtualHost>
-</IfModule>
-<IfModule mod_gnutls.c>
-<VirtualHost _default_:8443>
-    ServerName $host
-    DocumentRoot $destdir
-
-    <Directory "$destdir">
-        Options ExecCGI FollowSymLinks
-        AddHandler cgi-script .cgi
-        AllowOverride AuthConfig Limit
-        Require all granted
-    </Directory>
-
-    SSLEngine on
-    SSLCertificateFile "/secret/tls.crt"
-    SSLCertificateKeyFile "/secret/tls.key"
-</VirtualHost>
-</IfModule>
 EOT
+
+    if [ -n "$LDAP_HOST" ] && [ -n "$LDAP_SUFFIX" ]
+    then
+        cat << EOT >> $output
+
+    <Location /ikiwiki.cgi>
+        AuthType basic
+        AuthName "$wikiname Login"
+        AuthBasicProvider ldap
+        AuthLDAPUrl "ldaps://$LDAP_HOST/ou=People,$LDAP_SUFFIX?uid"
+        AuthLDAPGroupAttribute memberUid
+        AuthLDAPGroupAttributeIsDN off
+        Require valid-user
+
+        LDAPTrustedClientCert CERT_BASE64 /secret/tls.crt
+        LDAPTrustedClientCert KEY_BASE64 /secret/tls.key
+    </Location>
+EOT
+    fi
+
+    echo "<VirtualHost _default_:8080>" > /etc/apache2/sites-available/$host.conf
+    cat $output >> /etc/apache2/sites-available/$host.conf
+    echo "</VirtualHost>" >> /etc/apache2/sites-available/$host.conf
+    echo "<IfModule ssl_module>" >> /etc/apache2/sites-available/$host.conf
+    echo "<VirtualHost _default_:8443>" >> /etc/apache2/sites-available/$host.conf
+    cat $output >> /etc/apache2/sites-available/$host.conf
+    echo >> /etc/apache2/sites-available/$host.conf
+    echo "    SSLEngine on" >> /etc/apache2/sites-available/$host.conf
+    echo "    SSLCertificateFile \"/secret/tls.crt\"" >> /etc/apache2/sites-available/$host.conf
+    echo "    SSLCertificateKeyFile \"/secret/tls.key\"" >> /etc/apache2/sites-available/$host.conf
+    echo "</VirtualHost>" >> /etc/apache2/sites-available/$host.conf
+    echo "</IfModule>" >> /etc/apache2/sites-available/$host.conf
+    echo "<IfModule mod_gnutls.c>" >> /etc/apache2/sites-available/$host.conf
+    echo "<VirtualHost _default_:8443>" >> /etc/apache2/sites-available/$host.conf
+    cat $output >> /etc/apache2/sites-available/$host.conf
+    echo >> /etc/apache2/sites-available/$host.conf
+    echo "    SSLEngine on" >> /etc/apache2/sites-available/$host.conf
+    echo "    SSLCertificateFile \"/secret/tls.crt\"" >> /etc/apache2/sites-available/$host.conf
+    echo "    SSLCertificateKeyFile \"/secret/tls.key\"" >> /etc/apache2/sites-available/$host.conf
+    echo "</VirtualHost>" >> /etc/apache2/sites-available/$host.conf
+    echo "</IfModule>" >> /etc/apache2/sites-available/$host.conf
+
     /usr/sbin/a2ensite "$host"
 done
 
